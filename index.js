53d38a0ced17ccfd3f107d72323839990424360c
Kyle <kyleliang586@gmail.com>
2022-10-09 13:11:22 -0700

    commit tree 38be0d25af0e8a102b7d4b7dfe29c7674ebb75bd

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+//problem set #1
+let multiply
+const num1 = 31
+const num2 = 2
+    multiply = num1 * num2
+
+//problem set #2
+let random
+const numB = Infinity
+const generate = Math.ceil(Math.random())
+random = generate
+
+//problem set 3
+let mod
+const num3 = 4
+const num4 = 16
+    mod = num3 % num4
+//problem set 4
+
+let max
+const set = (5, 10, 15, 20)
+max = Math.max(set)
